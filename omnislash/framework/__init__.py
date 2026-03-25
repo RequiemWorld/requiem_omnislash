7bbf888dec9336f4185f5cc0ccc7f0662279b289
polllaris <polllaris@protonmail.com>
2026-03-25 02:16:24 -0400

    add start of a mechanism to point to values of resources on managed stacks

--- omnislash/framework/__init__.py
+++ omnislash/framework/__init__.py
@@ -6,6 +6,43 @@ Provisioner = Callable[[Self], None]
 TResource = TypeVar("TResource")
 
 
+@pulumi.input_type
+class StackComponentValueReference:
+	def __init__(self,
+				 stack_name: str,
+				 resource_type: type[pulumi.Resource],
+				 resource_name: str,
+				 property_name: str):
+		self._stack_name = stack_name
+		self._resource_type = resource_type
+		self._resource_name = resource_name
+		self._property_name = property_name
+
+	@property
+	def stack_name(self) -> str:
+		return self._stack_name
+
+	@property
+	def resource_type(self) -> type[pulumi.Resource]:
+		return self._resource_type
+
+	@property
+	def resource_name(self) -> str:
+		return self._resource_name
+
+	@property
+	def property_name(self) -> str:
+		return self._property_name
+
+
+class StackComponentError(Exception):
+	pass
+
+class MatchingResourceNotFound(StackComponentError):
+	pass
+
+
+
 class StackComponent:
 	_created_stack_components: list["StackComponent"] = list()
 	_resource_construction_info: list["ResourceConstructionInfo"] = list()
@@ -20,6 +57,23 @@ class StackComponent:
 
 	def add_provisioner(self, provisioner: Provisioner) -> None:
 		self._added_provisioners.append(provisioner)
+
+	def get_reference(self,
+					  resource_type: type[pulumi.Resource],
+					  resource_name: str,
+					  property_name: str) -> StackComponentValueReference:
+		"""
+		:raises MatchingResourceNotFound: When a resource matching the type and name can't be found within.
+		"""
+		for resource in self._created_resources:
+			has_matching_type = type(resource) is resource_type
+			has_matching_name = resource._name == resource_name
+			if has_matching_name and has_matching_type:
+				break
+		else:
+			raise MatchingResourceNotFound
+
+		return StackComponentValueReference(self.name, resource_type, resource_name, property_name)
 
 	def get_resources(self, resource_type: type[TResource]) -> list[TResource]:
 		return [resource for resource in self._created_resources if type(resource) is resource_type]
--- tests/test_stack_component.py
+++ tests/test_stack_component.py
@@ -2,6 +2,7 @@ import unittest
 from omnislash import StackComponent
 from pulumi_random import RandomId
 from pulumi_random import RandomString
+from omnislash.framework import MatchingResourceNotFound
 
 
 class TestStackComponentResources(unittest.TestCase):
@@ -22,3 +23,32 @@ class TestStackComponentResources(unittest.TestCase):
 		self._stack_component.add_resource(random_id)
 		self._stack_component.add_resource(random_string)
 		self.assertEqual([random_id], self._stack_component.get_resources(RandomId))
+
+
+class TestStackComponentValueReferences(unittest.TestCase):
+
+	def test_should_raise_matching_resource_not_found_when_trying_to_find_resource_on_empty_component(self):
+		empty_component = StackComponent("abc")
+		with self.assertRaises(MatchingResourceNotFound):
+			empty_component.get_reference(RandomString, "123", "property")
+
+	def test_should_raise_matching_resource_not_found_when_trying_to_find_resource_of_wrong_type_and_right_name(self):
+		component = StackComponent("555")
+		component.add_resource(RandomId("name", byte_length=4))
+		with self.assertRaises(MatchingResourceNotFound):
+			component.get_reference(RandomId, "value", "result")
+
+	def test_should_raise_matching_resource_not_found_when_trying_to_find_resource_of_wrong_name_and_right_type(self):
+		component = StackComponent("888")
+		component.add_resource(RandomString("ueval", length=8))
+		with self.assertRaises(MatchingResourceNotFound):
+			component.get_reference(RandomString, "lavue", "result")
+
+	def test_should_return_correct_value_reference_when_there_is_one_with_type_and_name_contained_within(self):
+		component = StackComponent("123")
+		component.add_resource(RandomString("string", length=4))
+		reference = component.get_reference(RandomString, "string", "result")
+		self.assertEqual("123", reference.stack_name)
+		self.assertIs(RandomString, reference.resource_type)
+		self.assertEqual("string", reference.resource_name)
+		self.assertEqual("result", reference.property_name)
